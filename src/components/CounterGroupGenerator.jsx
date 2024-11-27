bed0b38bf29a89bddaa358d172c5a910b9c34e1e
CHENJO20 <CHENJO20@oocl.com>
2024-11-27 17:27:57 +0800

    feat, add CounterGroupGenerator

---
+++ src/components/CounterGroupGenerator.jsx
@@ -0,0 +1,30 @@
+import {useState} from "react";
+import "./CounterGroupGenetor.css"
+
+const CounterGroupGenerator = (props) => {
+    const [size, setSize] = useState(0)
+    const handleChange = (e) => {
+        if(e.target.value < 0){
+            setSize(0)
+        }
+        else if(e.target.value > 20){
+            setSize(20)
+        }
+        else setSize(e.target.value);
+    };
+    const handleReset = () => {
+        if (size === props.size)
+            return;
+        props.setSize(size);
+    }
+    return (
+        <div className={"counter-group-generator-wrapper"}>
+            <span>size:</span>
+            <input min={0} max={20} type="number" value={size} onChange={handleChange}/>
+            <button onClick={handleReset}>reset</button>
+        </div>
+    )
+
+}
+
+export default CounterGroupGenerator;
